a4358f3eeeb6dcb6f7758b8066905ca67f9e628a
Poul Henriksen <pohe@easj.dk>
2019-09-24 12:50:20 +0200

    Add project files.

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            //int blankSpace = 0;
+            int row = 1;
+            while (row <= 10)
+            {
+                //blankSpace = 1;
+                //while (blankSpace < row)
+                //{
+                //    Console.Write(" ");
+                //    blankSpace++;
+                //}
+                for (int blankSpace = 1; blankSpace < row; blankSpace++)
+                {
+                    Console.Write(" ");
+                }
+                int col = 1;
+                while (col <= 11 - row)
+                {
+                    Console.Write((col % 2 == 0)? "|": "*");
+                    //if (col%2==0)
+                    //    Console.Write("|");
+                    //else
+                    //{
+                    //    Console.Write("*");
+                    //}
+                    col++;
+                }
+                Console.WriteLine();
+                row++;
+            }
+
+
+            Console.WriteLine("Hello world!");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
